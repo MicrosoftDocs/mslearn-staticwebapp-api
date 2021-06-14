174c6090ec5240ed1ebea0453667c3e148b6321c
John Papa <john@johnpapa.net>
2021-06-14 17:04:41 -0400

    angular 12 breaking change

--- angular-app/src/app/products/products.component.ts
+++ angular-app/src/app/products/products.component.ts
@@ -55,7 +55,7 @@ export class ProductsComponent implements OnInit {
   }
 
   add(product: Product) {
-    this.productService.add(product);
+    this.productService.add(product, {isOptimistic: false});
   }
 
   askToDelete(product: Product) {
